c02b301ea2baf431aa85c1839e5e8f374d634413
Egor Maleev <egor.maleev@list.ru>
2020-06-19 00:46:01 +0500

    egorka

---
+++ t-televisor/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t_televisor
+{
+    class TV
+    {
+        public static int Channel = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Текущий канал : 1" + "\nВсего каналов : 99" + "\nЕсли вы хотите переключить канал на следующий - нажмите Z" + "\nЕсли вы хотите переключить канал на предыдущий - нажмите X" + "\nЕсли вы хотите переключить канал по номеру - нажмите N\n");
+
+            string NextChannel;
+            while (true)
+            {
+                do
+                {
+                    NextChannel = Console.ReadLine();
+
+                    if (NextChannel == "X")
+                    {
+                        if (Channel == 1)
+                        {
+                            Channel = 31;
+                        }
+                        else
+                        {
+                            Channel--;
+                        }
+
+                        Console.WriteLine($"Вы переключили канал на {Channel}\n");
+                    }
+                    if (NextChannel == "Z")
+                    {
+                        if (Channel == 31)
+                        {
+                            Channel = 1;
+                        }
+                        else
+                        {
+                            Channel++;
+                        }
+                        Console.WriteLine($"Вы переключили канал на {Channel}\n");
+                    }
+                    if (NextChannel == "N")
+                    {
+                        Console.Write("Введите канал : ");
+                        int channel;
+                        bool isInt = int.TryParse(Console.ReadLine(), out channel);
+                        if (isInt)
+                            switchChannel(channel);
+                    }
+                    if ((NextChannel == "X") && (NextChannel == "Z") && (NextChannel == "N"))
+                    {
+                        Console.WriteLine("Введите X, Z или N\n");
+                    }
+                }
+                while ((NextChannel != "X") && (NextChannel != "Z") && (NextChannel != "N"));
+
+            }
+        }
+
+        static void switchChannel(int channel)
+        {
+            if (channel > 31)
+            {
+                channel = 31;
+            }
+            if (channel < 1)
+            {
+                channel = 1;
+            }
+
+            Channel = channel;
+
+            Console.WriteLine($"Вы переключили канал на {Channel}\n");
+        }
+
+    }
+}
